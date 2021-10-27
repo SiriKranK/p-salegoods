1864c3261e168a27f97c4bf22f193d26ba39c3e5
Sirikran krannate <poppurik@gmail.com>
2021-10-27 22:40:13 +0700

    re to set facebook

---
+++ Backend/functions/index.js
@@ -0,0 +1,20 @@
+const functions = require("firebase-functions");
+const express = require("express");
+// //const bodyParser = require("body-parser");
+const verifyWebhook = require("./verify-webhook");
+// //require("dotenv").config({ path: "./variables.env" });
+const messageWebhook = require("./message-webhook");
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", verifyWebhook);
+
+app.post("/", messageWebhook);
+
+app.listen(5000, () => console.log("Express server is listening on port 5000"));
+exports.app = functions.https.onRequest(app);
+// //npx nodemon
+
+
